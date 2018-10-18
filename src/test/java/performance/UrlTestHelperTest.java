a6dbfb23d71db1575cc7df48c369317be59290b0
EMC-CORP\nelio.martins <nelio.martins@emcconnected.com>
2018-10-18 12:03:26 -0400

    no message

--- src/test/java/performance/UrlTestHelperTest.java
+++ src/test/java/performance/UrlTestHelperTest.java
@@ -19,6 +19,7 @@ public class UrlTestHelperTest {
 	
 	@Test
 	public void testHTTP200() throws Exception {
+		System.out.println("Running testHTTP200()");
 		URL url = new URL("https://api-dev-01.emcconnected.com/testapi/US/area");
 		UrlTestResults results = urlTestHelper.test(url);
 		assertTrue(results.getHttpResponseHeaders().startsWith("HTTP/1.1 200") && results.getHttpBodySize()==39);
@@ -26,6 +27,7 @@ public class UrlTestHelperTest {
 	
 	@Test
 	public void testHTTP404() throws Exception {
+		System.out.println("Running testHTTP404()");
 		URL url = new URL("https://api-dev-01.emcconnected.com/invalid-file");
 		UrlTestResults results = urlTestHelper.test(url);
 		assertTrue(results.getHttpResponseHeaders().startsWith("HTTP/1.1 404"));
@@ -33,6 +35,7 @@ public class UrlTestHelperTest {
 
 	@Test
 	public void testServerAddress() throws Exception {
+		System.out.println("Running testServerAddress()");
 		URL url = new URL("https://api-qa-01.emcconnected.com/waittests/?wait=1");
 		UrlTestResults results = urlTestHelper.test(url);
 		assertTrue("50.19.195.96".equals(results.getServerAddress()));
@@ -40,6 +43,7 @@ public class UrlTestHelperTest {
 	
 	@Test
 	public void testHTTPContentType() throws Exception {
+		System.out.println("Running testHTTPContentType()");
 		URL url = new URL("https://api-qa-01.emcconnected.com/static/test.txt");
 		UrlTestResults results = urlTestHelper.test(url);
 		assertTrue(results.getHttpResponseHeaders().contains("text/plain"));
@@ -48,6 +52,7 @@ public class UrlTestHelperTest {
 	
 	@Test
 	public void testHTTPContentLength() throws Exception {
+		System.out.println("Running testHTTPContentLength()");
 		int size = (int)(Math.random()*1000);
 		URL url = new URL("https://api-qa-01.emcconnected.com/waittests/?wait=1-"+size+"&responseBodyType=fixed");
 		UrlTestResults results = urlTestHelper.test(url);
@@ -57,6 +62,7 @@ public class UrlTestHelperTest {
 	
 	@Test
 	public void testBodyTime() throws Exception {
+		System.out.println("Running testBodyTime()");
 		int delay = (int)(Math.random()*5) + 1; //1-5 secs
 		URL url = new URL("https://api-qa-01.emcconnected.com/waittests/?wait="+delay+"-100&responseBodyType=fixed");
 		UrlTestResults results = urlTestHelper.test(url);
@@ -66,12 +72,14 @@ public class UrlTestHelperTest {
 	
 	@Test(expected=UnknownHostException.class)
 	public void testUnknownHost() throws Exception {
+		System.out.println("Running testUnknownHost()");
 		URL url = new URL("https://invalidUrl.unexisting-host-here.com");
 		urlTestHelper.test(url);
 	}
 
 	@Test(expected=IOException.class)
 	public void testIOException() throws Exception {
+		System.out.println("Running testIOException()");
 		int invalidPort = 8983;
 		URL url = new URL("https://api-dev-01.emcconnected.com:"+invalidPort+"/testapi/US/area");
 		urlTestHelper.test(url);		
